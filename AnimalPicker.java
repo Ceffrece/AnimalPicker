4a16a448929ac2115ea9aa9a025fcf0a3d49eddd
Ceffrece <cheffrece@quinnipiac.edu>
2023-02-21 19:16:47 -0500

    Adds an option to pick dog or cat

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class AnimalPicker {
   public static void main(String[] args){
-
+    Scanner scan = new Scanner(System.in);
+    boolean loopActive = true;
+    while(loopActive){
+        System.out.println("Would you like to see a dog or cat? (Use 'cat' for cat and 'dog' for dog)");
+        String choice = scan.nextLine().toLowerCase();
+        switch(choice){
+            case("cat"): System.out.println("Cat ACII art here");
+            break;
+            case("dog"): System.out.println("Dog ACII art here");
+            break;
+        }
+    }
+    
   }
 }
