9b35b50fa4886cff47ff267c06a513e37609323e
Momoreoluwa Ayinde <maayinde@quinnipiac.edu>
2023-02-22 20:55:01 -0500

    added duck option

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,7 +4,7 @@ public class AnimalPicker {
     Scanner scan = new Scanner(System.in);
     boolean loopActive = true;
     while(loopActive){
-        System.out.println("Would you like to see a dog or cat? (Use 'cat' for cat and 'dog' for dog)");
+        System.out.println("Would you like to see a dog , cat, fish or cow? (Use 'cat' for cat, 'dog' for dog, 'fish' for fish, 'cow' for cow)))");
         String choice = scan.nextLine().toLowerCase();
         switch(choice){
             case("cat"): System.out.println("          _       \n   ___ __ _| |_ ___ \n  / __/ _` | __/ __|\n | (_| (_| | |_\\__ \\\n  \\___\\__,_|\\__|___/");
@@ -12,8 +12,11 @@ public class AnimalPicker {
             case("dog"): System.out.println("     _                 \n   __| | ___   __ _ ___ \n  / _` |/ _ \\ / _` / __|\n | (_| | (_) | (_| \\__ \\\n  \\__,_|\\___/ \\__, |___/\n              |___/     ");
             break;
             case("fish"): System.out.println("   __ _     _     \n  / _(_)___| |__  \n | |_| / __| \'_ \\ \n |  _| \\__ \\ | | |\n |_| |_|___/_| |_|");
+            break;
+            case("cow"): System.out.println("          __n__n__\n    .------`-\\00/-\'\n   /  ##  ## (oo)\n  / \\## __   ./\n     |//YY \\|/\nsnd  |||   |||");
             default: System.out.println("ERROR: Please enter a correct value");
             break;
+
         }
     }
     
